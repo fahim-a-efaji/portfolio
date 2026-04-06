ffa08054b4e9db36c51226184142230181f9e477
fahim-a-efaji <159943140+fahim-a-efaji@users.noreply.github.com>
2026-04-06 15:26:17 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/portfolio/", // <- important for GitHub Pages project repo
   plugins: [react()],
-  base: './',          // works on GitHub Pages subdirectory deploys
-})
+});
